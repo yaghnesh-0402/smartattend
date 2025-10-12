8dd8827b38b407265eeecbf17d8f8aa9e238d36b
yaghnesh mayavan <yaghneshmayavan2005@gmail.com>
2025-10-12 17:21:14 +0000

    the barcode id also contains alphabets so the barcode isnt giving the ri

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,7 @@ export default function SmartAttend() {
               },
             },
             decoder: {
-              readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
+              readers: ["code_128_reader", "code_39_reader"]
             },
             locate: true,
           }, (err) => {
