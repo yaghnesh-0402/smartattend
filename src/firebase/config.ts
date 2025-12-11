6a21a6dc70671b75c1e75f684d8f382ac0a0e7b7
yaghnesh-0402 <yaghneshmayavan2005@gmail.com>
2025-12-11 22:27:59 +0530

    Update Firebase configuration credentials
    Updated Firebase configuration with new appId and apiKey.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3604423653-852bb",
-  "appId": "1:586022553161:web:6d7d16f4b5d40aaaf52198",
-  "apiKey": "AIzaSyASL980SSAe0HNtetwsjmb_-U8FJf-H2BA",
+  "appId": "add urs",
+  "apiKey": "add urs",
   "authDomain": "studio-3604423653-852bb.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "586022553161"
